6f3393c1bd2f6a07086d61d24e84cefcfc795bb4
unknown <vysotskiidanil@gmail.com>
2023-07-15 17:20:31 +0600

    update

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
-import pytz
+import asyncio
+import aioschedule
 import requests
 import telebot
+import logging
 from datetime import datetime
+from telebot.async_telebot import AsyncTeleBot
+
 from bot_data import token
 
 
-def get_24hr_data(coin):
-    req = requests.get(f"https://api.binance.com/api/v3/ticker/24hr?symbol={str(coin).upper()}USDT")
-    res = dict(req.json())
-    tz = pytz.timezone("UTC")
-    date = datetime.now(tz).strftime("%Y-%m-%d %H:%M UTC")
-    return (
-        f"DATE --> {date}\nNAME --> {res['symbol']}\nPRICE --> {round(float(res['lastPrice']), 4)}\n"
-        f"PRICE CHANGE --> {round(float(res['priceChange']), 4)}")
+def get_coin_market(coin):
+    request = requests.get(f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coin}")
+    response = dict(request.json()[0])
+    name = response.get("name")
+    current_price = response.get("current_price")
+    market_cap = response.get("market_cap")
+    total_volume = response.get("total_volume")
+    price_change_24h = response.get("price_change_24h")
+    return f"{name}\nprice -> {current_price}\nprice change -> {price_change_24h}" \
+           f"\nmkt cap -> {market_cap}\nvolume -> {total_volume}"
 
 
-def telegram_bot(token):
-    bot = telebot.TeleBot(token)
+bot = AsyncTeleBot(token)
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+now = datetime.now()
+current_time = now.strftime("%H:%M")
 
-    @bot.message_handler(commands=["start"])
-    def start_message(message):
-        bot.send_message(message.chat.id,
-                         "Hello friend!\nWrite the token name to see its price\nExample:    eth, btc or matic")
-        bot.send_message(message.chat.id,
-                         "Привет, друг!\nНапиши название токена, чтобы увидеть его цену\nПример:  eth, btc или matic")
 
-    @bot.message_handler(content_types=["text"])
-    def send_text(message):
-        try:
-            bot.send_message(message.chat.id, get_24hr_data(message.text))
-        except Exception as ex:
-            print(ex)
-            bot.send_message(message.chat.id, "Damn...Something was wrong...")
+async def send_info(chat_id) -> None:
+    await bot.send_message(chat_id, get_coin_market("bitcoin"))
+    aioschedule.clear(chat_id)
 
-    bot.polling()
+
+@bot.message_handler(commands=["start"])
+async def send_welcome(message):
+    await bot.reply_to(message, "Hi!")
+
+
+@bot.message_handler(commands=["market"])
+async def set_timer(message):
+    aioschedule.every(3600).seconds.do(send_info, message.chat.id).tag(message.chat.id)
+
+
+async def scheduler():
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
+async def main():
+    await asyncio.gather(bot.infinity_polling(), scheduler())
 
 
 if __name__ == "__main__":
-    telegram_bot(token)
+    asyncio.run(main())
