b42c85cb8b412682922ca30cbda94644e7799111
radioactive1337 <vysotskiidanil@gmail.com>
2024-01-30 19:53:45 +0600

    l2

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
-from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from dotenv import find_dotenv, load_dotenv
 
-load_dotenv()
+from handlers.user_handlers import user_router
+
+load_dotenv(find_dotenv())
+ALLOWED_UPDATES = ['message, edited_message']
+
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Это была команда старт')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    text = message.text
-
-    if text in ['test']:
-        await message.answer('test')
-    else:
-        await message.answer(message.text)
+dp.include_routers(user_router)
 
 
 async def main():
-    await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 asyncio.run(main())
